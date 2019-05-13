bf12350898810784e0bbdc788544cccdbfff2638
Reed James <ReedTJames1234@gmail.com>
2019-05-13 17:45:50 -0500

    Commit before standup

--- js/index.js
+++ js/index.js
@@ -59,5 +59,50 @@ banner.setAttribute("src", siteContent["cta"]["img-src"]);
 // Using your selectors, update the content to match the example file.
 // Remember to update the src attributes on images
 
-let navItem = document.querySelectorAll("nav a");
-console.log(navItem);
+let navItem = document.querySelectorAll("a");
+navItem[0].textContent = siteContent["nav"]["nav-item-1"];
+navItem[1].textContent = siteContent["nav"]["nav-item-2"];
+navItem[2].textContent = siteContent["nav"]["nav-item-3"];
+navItem[3].textContent = siteContent["nav"]["nav-item-4"];
+navItem[4].textContent = siteContent["nav"]["nav-item-5"];
+navItem[5].textContent = siteContent["nav"]["nav-item-6"];
+
+let headerOne = (document.querySelector(".cta .cta-text h1").textContent =
+  "Dom is Awesome");
+
+let button = (document.querySelector(".cta .cta-text button").textContent =
+  "Get Started");
+
+let mainContent = document.querySelectorAll("h4");
+mainContent[0].textContent = siteContent["main-content"]["features-h4"];
+mainContent[1].textContent = siteContent["main-content"]["about-h4"];
+mainContent[2].textContent = siteContent["main-content"]["services-h4"];
+mainContent[3].textContent = siteContent["main-content"]["product-h4"];
+mainContent[4].textContent = siteContent["main-content"]["vision-h4"];
+
+let mainText = document.querySelectorAll("p");
+mainText[0].textContent = siteContent["main-content"]["features-content"];
+mainText[1].textContent = siteContent["main-content"]["about-content"];
+mainText[2].textContent = siteContent["main-content"]["services-content"];
+mainText[3].textContent = siteContent["main-content"]["product-content"];
+mainText[4].textContent = siteContent["main-content"]["vision-content"];
+
+let midImg = document.getElementById("middle-img");
+midImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+let contact = document.querySelectorAll(".contact");
+contact[0].textContent = siteContent["contact"]["contact-h4"];
+
+// let contactText = document.querySelectorAll(".contact p");
+// contactText[0].textContent = siteContent["contact"]["address"];
+// contactText[1].textContent = siteContent["contact"]["phone"];
+// contactText[2].textContent = siteContent["contact"]["email"];
+
+// #########################################################
+// Change the color of the navigation text to be green.
+// Utilize.appendChild() and.prepend() to add two new items to the navigation system.You can call them whatever you want.
+// Check your work by looking at the original html in the browser
+let green = document.querySelectorAll("header nav a");
+green.forEach(aGreen => {
+  aGreen.style.color = "green";
+});
